93169aa7d419bd20ba3c74f42be633384a092458
dat2000-dev <53572868+dat2000-dev@users.noreply.github.com>
2021-07-28 00:48:04 +0700

    Create demo.c

---
+++ TRAINING/demo.c
@@ -0,0 +1,27 @@
+*&---------------------------------------------------------------------*
+*& Report Z_DEMO
+*&---------------------------------------------------------------------*
+*&
+*&---------------------------------------------------------------------*
+REPORT Z_DEMO.
+
+* Tao mot man hinh chua cac thong tin ca nhan su dung "WRITE and '/' "
+* Example:
+* ------------------------------------------------
+* FULL NAME: Trinh Xuan Dat                       |
+* DATE: 19-07-2000                                |
+* ADD: Ha Dong - Ha Noi                           |
+* PHONE NUMBER: 0332647020                        |
+* PROGRAMMING LANGUAGE: C/C++ , ABAP, JAVA, PYTHON|
+* COMPETENTLY: C/C++                              |
+* ACHIEVEMENTS: ACM/ICPC, OLYMPIC                 |
+* INTEREST: Play game, Coding, picnic             |
+*  ------------------------------------------------
+
+WRITE: 'FULL NAME: Trinh Xuan Dat', / ,
+       'DATE: 19-07-2000   ',/,
+       'PHONE NUMBER: 0332647020',/,
+       'PROGRAMMING LANGUAGE: C/C++ , ABAP, JAVA, PYTHON',/,
+       'COMPETENTLY: C/C++  ',/,
+       'ACHIEVEMENTS: ACM/ICPC, OLYMPIC  ',/,
+       'INTEREST: Play game, Coding, picnic  ',/.
